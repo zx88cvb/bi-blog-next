d45a716847769af2415bafa0ab1e1ce7380acc2f
Bi <295389754@qq.com>
2025-08-19 22:21:54 +0800

    feat: console

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -16,6 +16,8 @@ interface GoogleAnalyticsProps {
 
 export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
   useEffect(() => {
+    console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+    console.log("gaId", gaId)
     // 只在生产环境加载
     if (process.env.NODE_ENV !== 'production') {
       return
