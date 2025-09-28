e882112715edec8aa00b092264257ecc53a121e6
Bi <295389754@qq.com>
2025-09-28 17:13:00 +0800

    feat: robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -11,6 +11,7 @@ export default function robots(): MetadataRoute.Robots {
         '/api/',
         '/_next/',
         '/private/',
+        '/cdn-cgi/'
       ],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
